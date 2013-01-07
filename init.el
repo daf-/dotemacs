b16f763ae0c06bd3d5a7cb1e324702261ad8d0be
Dan Friedman <dfriedma@saruman.cs.oberlin.edu>
2013-01-07 18:46:31 -0500

    add package.el

--- init.el
+++ init.el
@@ -10,6 +10,16 @@
 ;; ------- PATH AND REQUIRES ------- ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
+
 ;; run emacs in server-mode, so that new sessions start quickly
 ;; (server-start)
 
