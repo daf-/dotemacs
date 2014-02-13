c5edc0a595e15ebb1f3da030d09e03b80743f398
Dan Friedman <dfriedma@oberlin.edu>
2014-02-13 17:05:49 -0500

    Properly set up exec path from shell...

--- config/osx.el
+++ config/osx.el
@@ -5,4 +5,8 @@
 (setq mac-command-modifier 'meta)
 (setq ns-function-modifier 'hyper)
 
+;; exec-path-from-shell
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
 (provide 'osx)
